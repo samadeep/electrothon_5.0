78ba83d90756ae58acd70cf4d583ff01d8ac0e43
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2022-09-14 01:47:24 +0530

    mrigesh

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
+import "./header.css";
+
+const Header = () => (
+  <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header-content">
+      <h1 className="gradient__text">Electrothon 5.0</h1>
+      <p>Search | Plan | Engage | Create</p>
+      <p>18th-20th Feb, 2023</p>
+
+      <div className="gpt3__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Go to Dashboard</button>
+      </div>
+
+      <div className="gpt3__header-content__people">
+        <img src={people} />
+        <p>160 people registered in last 24 hours</p>
+      </div>
+    </div>
+
+    <div className="gpt3__header-image">
+      <img src={ai} />
+    </div>
+  </div>
+);
+
+export default Header;
